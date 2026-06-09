014f34877ae471d9091f62d014ea268aa3c8d7b0
unknown <francesco.diiorio2008@gmail.com>
2026-06-09 12:21:57 +0200

    aggiunta base per classe combattimento

--- tracker.py
+++ tracker.py
@@ -33,3 +33,32 @@ class Combattente:
     def aggiungere_hp_temporanei(self, hp):
         if hp > self.hpTemporanei:
             self.hpTemporanei = hp
+
+    def aggiornare_condizioni(self):
+        for condizione in self.condizioni:
+            condizione['Durata'] -= 1
+        self.condizioni = [c for c in self.condizioni if c['Durata'] > 0]
+
+# Classe per gestire il combattimento
+class Combattimento:
+    def __init__(self):
+        self.combattenti = []
+        self.turno_attuale = 0
+        self.round_attuale = 1
+
+    def aggiungere_combattente(self, combattente):
+        self.combattenti.append(combattente)
+        self.ordinare_iniziativa()
+
+    def ordinare_iniziativa(self):
+        self.combattenti.sort(key=lambda c: c.iniziativa, reverse=True)
+
+    def prossimo_turno(self):
+        if not self.combattenti:
+            return None
+        combattente_corrente = self.combattenti[self.turno_attuale]
+        combattente_corrente.aggiornare_condizioni()
+        self.turno_attuale = (self.turno_attuale + 1) % len(self.combattenti)
+        if self.turno_attuale == 0:
+            self.round_attuale += 1
+        return combattente_corrente
